b05c6131820827f28f8aade20fadd1a9a6dca30e
Kevin <kevirl@gmail.com>
2016-11-24 23:03:10 +0000

    Added Login functions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    user = Account.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+    log_in user
+      redirect_to user
+    else
+      # Display error message
+      flash.now[:danger] = 'Invalid email Id / Password'
+      render 'new'
+    end
+  end
+  
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+  
+  def accInfo
+    redirect_to current_user
+  end
+  
+  
+end
